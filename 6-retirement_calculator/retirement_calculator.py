153f2c489c1857518083f4d81ae473a1cb7dbc2c
Sachintha Dinuranga <117833055+Sachintha-Dinuranga@users.noreply.github.com>
2024-12-10 13:41:41 +0530

    Challenge 6

---
+++ 6-retirement_calculator/retirement_calculator.py
@@ -0,0 +1,12 @@
+from datetime import date
+
+current_age = input("What is your current age? ")
+retirement_age = input("At what age would you like to retire? ")
+
+todays_date = date.today()
+current_year = todays_date.year
+years_left = int(retirement_age) - int(current_age)
+retirement_year = todays_date.year + years_left
+
+print("You have " + str(years_left) + " years left until you can retire.")
+print("It's " + str(current_year) + ", so you can retire in " + str(retirement_year) + ".")
